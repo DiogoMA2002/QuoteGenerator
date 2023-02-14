537d70bee3d580bb36cc4d91c71bec8071971ea8
Diogo Andrade <100608872+DiogoMA2002@users.noreply.github.com>
2023-02-14 16:56:02 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def verQuote():
     file = obternome()
     with open(file, 'r') as fp:
         content = fp.readlines()
-        print(content)
     print(content[i])
 
 def getNumLines():
@@ -33,15 +32,15 @@ def getNumLines():
         numLinhas = len(fp.readlines())
         i = random.randint(0, numLinhas-1)
     return i
-
-
-opcao = input("Insira a Opção desejada:\n0-Criar novo ficheiro\n1-Inserir Quote\n2-Ver uma quote\n3-Sair\n> ")
-if(opcao == "0"):
-    criarFicheiro()
-elif(opcao == "1"):
-    inserirQuote()
-elif(opcao == "2"):
-    verQuote()
-else:
-    exit()
-    
+opcao = 0
+while(opcao != "1" or opcao != "2" or opcao != "0"):
+    opcao = input("Insira a Opção desejada:\n0-Criar novo ficheiro\n1-Inserir Quote\n2-Ver uma quote\n3-Sair\n> ")
+    if(opcao == "0"):
+        criarFicheiro()
+    elif(opcao == "1"):
+        inserirQuote()
+    elif(opcao == "2"):
+        verQuote()
+    else:
+        exit()
+        
